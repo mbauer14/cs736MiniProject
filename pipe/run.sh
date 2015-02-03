d3236bec0b62dfb769385250a72ce5f2b6ec8fbc
Justin Moeller <jgmoeller@wisc.edu>
2015-02-03 14:21:49 -0600

    Added latency files

---
+++ pipe/run.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+NUM_ITERATIONS=10
+
+SIZE=4
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
+
+SIZE=16
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
+
+SIZE=64
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
+
+SIZE=256
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
+
+SIZE=1024
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
+
+SIZE=4096
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
+
+SIZE=16384
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
+
+SIZE=655536
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
+
+SIZE=262144
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
+
+SIZE=524288
+rm -f latency_$SIZE.txt
+for((c=1; c<=$NUM_ITERATIONS; c++))
+do
+  ./runpipe $SIZE >> latency_$SIZE.txt
+done
